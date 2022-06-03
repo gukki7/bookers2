2f2b0a11087b3b58a77597eca577cc1afbd169c5
EC2 Default User <ec2-user@ip-172-31-6-170.ec2.internal>
2022-06-03 14:11:59 +0000

    [Add] 投稿に関するフラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,21 @@
 class BooksController < ApplicationController
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id) , notice: 'successfully'
+    else
+    render :index
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: 'successfully'
+    else
+    render :edit
+    end
+  end
 end

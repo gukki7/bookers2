effe76f494c1a7a5c585c499cf84360d9bd573cc
EC2 Default User <ec2-user@ip-172-31-6-170.ec2.internal>
2022-06-12 14:33:44 +0000

    [Add] アソシエーションの記載

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_102907) do
+ActiveRecord::Schema.define(version: 2022_06_12_063924) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
